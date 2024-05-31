a5da93e0f2a38bc84038214010c69db3914c096a
basalaevaa <140111722+basalaevaa@users.noreply.github.com>
2024-05-31 13:49:44 +0300

    Add files via upload

---
+++ src/components/base/Api.ts
@@ -0,0 +1,58 @@
+export interface PageInterface {
+	counter: number;
+	gallery: HTMLElement[];
+	locked: boolean;
+}
+
+export type ApiListResponse<Type> = {
+    total: number,
+    items: Type[]
+};
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+export interface ApiResponse {
+    total: number;
+    items: PageInterface[];
+  }
+
+
+export class Api {
+    readonly baseUrl: string;
+    protected options: RequestInit;
+
+    constructor(baseUrl: string, options: RequestInit = {}) {
+        this.baseUrl = baseUrl;
+        this.options = {
+            headers: {
+                'Content-Type': 'application/json',
+                ...(options.headers as object ?? {})
+            }
+        };
+    }
+
+    protected async handleResponse(response: Response): Promise<object> {
+        if (response.ok) return response.json();
+        else {
+            const data = await response.json();
+            return await Promise.reject(data.error ?? response.statusText);
+        }
+    }
+
+    async get(uri: string) {
+        const response = await fetch(this.baseUrl + uri, {
+            ...this.options,
+            method: 'GET',
+        });
+        return this.handleResponse(response);
+    }
+
+    async post(uri: string, data: object, method: ApiPostMethods = 'POST') {
+        const response = await fetch(this.baseUrl + uri, {
+            ...this.options,
+            method,
+            body: JSON.stringify(data),
+        });
+        return this.handleResponse(response);
+    }
+}
